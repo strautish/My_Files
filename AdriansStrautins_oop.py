9be77cda6ec19e9442af8f0d78cf5ab5dc98f2c6
strautish <adrians.janis.strautins@edu.tukums.lv>
2025-10-29 15:15:25 +0200

    Jedzienu skaidrojums

--- AdriansStrautins_oop.py
+++ AdriansStrautins_oop.py
@@ -5,5 +5,11 @@ logs=tk.Tk()
 logs.title("Fizika")
 logs.geometry("500x500+700+300") #izveido logu
 
+jedziens1=tk.Label(logs, text="p - Spiediens", font=('Arial',10,'bold'), padx=100, pady=10, width=13, height=1)
+jedziens1.pack()
+jedziens2=tk.Label(logs, text="F - Spiediena spēks", font=('Arial',10,'bold'), padx=100, pady=10, width=13, height=1)
+jedziens2.pack()
+jedziens3=tk.Label(logs, text="S - laukums", font=('Arial',10,'bold'), padx=100, pady=10, width=13, height=1)
+jedziens3.pack() #pievieno jēdzienu skaidrojumu
 
 logs.mainloop() 
